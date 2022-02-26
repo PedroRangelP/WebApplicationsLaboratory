018aaf9925d8e7bbf404a099b2333c7d3c315ac8
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2022-02-25 22:49:56 -0600

    Homework 1

---
+++ Node.js Modules/operaciones.js
@@ -0,0 +1,19 @@
+exports.producto = (a,b) => {
+  return a*b
+}
+
+exports.suma = (a,b) => {
+  return a+b
+}
+
+exports.resta = (a,b) => {
+  return a-b
+}
+
+exports.division = (a,b) => {
+  return a/b
+}
+
+exports.modulo = (a,b) => {
+  return a%b
+}
