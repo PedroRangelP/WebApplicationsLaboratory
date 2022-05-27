e2b1105effd1035aeaf21b7304313c14a143e362
Pedro Rangel <42298267+PedroRangelP@users.noreply.github.com>
2022-05-27 17:31:31 -0500

    Added sendgrid

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -10,3 +10,43 @@ const mailConfig = {
 }
 
 module.exports = nodemailer.createTransport(mailConfig)
+
+// const dotenv = require('dotenv');
+// dotenv.config();
+// const nodemailer = require('nodemailer')
+// const sgTransport = require('nodemailer-sendgrid-transport')
+ 
+// let mailConfig;
+ 
+// if (process.env.NODE_ENV == "production") {
+//     const options = {
+//         auth: {
+//             api_key: process.env.SENDGRID_API_KEY
+//         }
+//     }
+//     mailConfig = sgTransport(options)
+// }
+// else {
+//     if (process.env.NODE_ENV == "staging") {
+//         console.log('XXXXXXXXXXXXXXXXXXXX')
+//         const options = {
+//             auth: {
+//                 api_key: process.env.SENDGRID_API_KEY
+//             }
+//         }
+//         mailConfig = sgTransport(options)
+//     }
+//     else {
+//         //ethereal (local)
+//         mailConfig = {
+//             host: 'smtp.ethereal.email',
+//             port: 587,
+//             auth: {
+//                 user: process.env.ETHEREAL_USER,
+//                 pass: process.env.ETHEREAL_PASSWORD
+//             }
+//         }
+//     }
+// }
+ 
+// module.exports = nodemailer.createTransport(mailConfig)
